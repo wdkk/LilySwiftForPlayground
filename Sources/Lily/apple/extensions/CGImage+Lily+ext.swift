947e52cb22e66d937ac8b664491d9f0f3f8703d0
Kengo Watanabe <kengo@wdkk.co.jp>
2024-02-12 00:20:35 +0900

    テクスチャアトラスの高速化

---
+++ Sources/Lily/apple/extensions/CGImage+Lily+ext.swift
@@ -0,0 +1,37 @@
+//
+// LilySwift Library Project
+//
+// Copyright (c) Watanabe-Denki, Inc. and Kengo Watanabe.
+//   https://wdkk.co.jp/
+//
+// This software is released under the MIT License.
+//   https://opensource.org/licenses/mit-license.php
+//
+
+import Foundation
+import QuartzCore
+#if os(macOS)
+import AppKit
+#else
+import UIKit
+#endif
+
+extension CGImage
+{
+    public static func load( assetName:String ) -> CGImage? {
+        #if os(macOS)
+        return NSImage( named:assetName )?.cgImage
+        #else
+        return UIImage( named:assetName )?.cgImage
+        #endif
+    }
+    
+    public var bytes:UnsafePointer<UInt8>? {
+        guard let provider = self.dataProvider else { return nil }
+        return CFDataGetBytePtr( provider.data )
+    }
+    
+    public var size:LLSizeInt {
+        return .init( self.width, self.height )
+    }
+}
