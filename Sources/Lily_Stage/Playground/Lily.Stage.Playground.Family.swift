156d828a2ac7a4c08146c5e8685cfed7e5eb2307
Kengo Watanabe <kengo@wdkk.co.jp>
2024-02-13 03:54:43 +0900

    Playground2DとPlayground3Dを統合し, Playgroundに改名

---
+++ Sources/Lily_Stage/Playground/Lily.Stage.Playground.Family.swift
@@ -0,0 +1,25 @@
+//
+// LilySwift Library Project
+//
+// Copyright (c) Watanabe-Denki, Inc. and Kengo Watanabe.
+//   https://wdkk.co.jp/
+//
+// This software is released under the MIT License.
+//   https://opensource.org/licenses/mit-license.php
+//
+
+import Metal
+
+extension Lily.Stage
+{
+    open class Playground
+    {
+        open class Plane { }
+ 
+        open class Billboard { }
+        
+        open class Model { }
+        
+        open class sRGB { }
+    }
+}
