47506708f0793483747deab9277177394ffa999b
Kengo Watanabe <kengo@wdkk.co.jp>
2024-03-21 03:22:39 +0900

    LilySwiftに同期

--- Supports/PGAlias.swift
+++ Supports/PGAlias.swift
@@ -8,9 +8,10 @@
 //   https://opensource.org/licenses/mit-license.php
 //
 
-// LilySwift利用時のサポート(Playgroundリポジトリになるときは置き換わる)
+// LilySwift利用時のエイリアスサポートをする
+// PlaygroundリポジトリではLilySwiftForPlayground置き換わる
+// Booksではimportが消えて同一モジュールとなる
 import LilySwiftForPlayground
-
 
 public typealias PG = Lily.Stage.Playground
 public typealias PGScreen = PG.PGScreen
